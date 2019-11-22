1774534c5c2299c6d8df033b4f2aef34e0634a24
TotKtoNeNado <TotKtoNeNado@users.noreply.github.com>
2019-11-22 15:58:52 +0300

    hw6

--- hw6/provision.sh
+++ hw6/provision.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 
-
 #Скачиваем дистрибутив
 
 cd /home/builder
@@ -20,30 +19,37 @@ rpm -Uvh nginx-1.14.1-1.el7_4.ngx.src.rpm
 cat nginx.spec > /home/builder/rpmbuild/SPECS/nginx.spec
 
 #Ставим все зависимости
-yum-builddep /home/builder/rpmbuild/SPECS/nginx.spec
+sudo yum-builddep /home/builder/rpmbuild/SPECS/nginx.spec -y
 
 #Собираем пакет
 rpmbuild -ba /home/builder/rpmbuild/SPECS/nginx.spec
 
-
+#Ставим собранный nginx
 sudo yum localinstall  /home/builder/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm -y
 
-
+#Создаем папку репозитория
 sudo mkdir -p /usr/share/nginx/html/repo
 
+#Копираем созданый пакет в репозиторий
 sudo cp /home/builder/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
 
+#Качаем ещё рдин пакет для репозитория
 sudo wget http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-0.1-6.noarch.rpm
 
+#Создаем репозиторий
 sudo createrepo /usr/share/nginx/html/repo/
 
+#Включаем в nginx листинг дириктории
 sudo sed '/index.htm;/i autoindex on; ' /etc/nginx/conf.d/default.conf 
 
+#Проверяем конфигурацию и стартуем
 sudo nginx -t
 sudo systemctl start nginx
+sllep 5
 sudo systemctl status nginx
 
-sudo cat /home/builder/otus.repo /etc/yum.repos.d/otus.repo 
+#Добавим наш репозиторий в /etc/yum.repos.d
+sudo cp -f /home/builder/otus.repo /etc/yum.repos.d/
 
 yum repolist enabled | grep otus
-yum list | grep otus
+
